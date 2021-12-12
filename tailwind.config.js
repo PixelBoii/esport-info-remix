0d03dadfc1f379d56a9a3854929173ed4498e9f8
PixelBoii <priwer.kontakt@gmail.com>
2021-12-12 23:10:28 +0100

    feat: Update gray palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./app/**/*.{ts,tsx}"],
   theme: {
     extend: {
       colors: {
+        gray: colors.neutral,
         primary: '#A72603',
       }
     },
